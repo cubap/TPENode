ad0362fe098b5e792cb8c2207bf6511d5e1c6e82
Patrick Cuba <cubap@slu.edu>
2021-01-22 13:23:37 -0600

    broop

--- public/transcribe/image-components.js
+++ public/transcribe/image-components.js
@@ -134,8 +134,59 @@ class RrViewbox extends HTMLElement {
                     loadImage()
                 })
         }
+        const navigateHandler = event => {
+            const toNextLineId = (el) => {
+                const c = this.presentation.canvas
+                const currentLine = el.line
+                const newLine = c.otherContent[0].resources[c.otherContent[0].resources.findIndex(i => i['@id'] === currentLine) + 1]
+                if (!newLine) {
+                    // throw Error("There is no next line on this list.")
+                }
+                toLine(el, newLine['@id'])
+            }
+            const toPreviousLineId = (el) => {
+                const c = this.presentation.canvas
+                const currentLine = el.line
+                const newLine = c.otherContent[0].resources[c.otherContent[0].resources.findIndex(i => i['@id'] === currentLine) - 1]
+                if (!newLine) {
+                    // throw Error("There is no previous line on this list.")
+                }
+                toLine(el, newLine['@id'])
+            }
+            const toLine = (el, id) => {
+                if (typeof id !== "string") {
+                    throw Error("This is not an id or uri that I recognize:", id)
+                }
+                el.setAttribute('rr-line', id)
+            }
+            const goto = event.detail.goto
+            switch (goto.type) {
+                case "line":
+                    if (this.presentation.canvas) {
+                        const lineElem = typeof this.line === "string" ? this : this.closest('[rr-line]')
+                        switch (goto.id) {
+                            case "next":
+                                toNextLineId(lineElem)
+                                break
+                            case "previous":
+                                toPreviousLineId(lineElem)
+                                break
+                            default:
+                                toLine(lineElem, goto.id)
+                        }
+                    } else {
+                        console.error("No Canvas is stored in memory.")
+                    }
+                    break
+                case "canvas":
+                    const canvasElem = typeof this.canvas === "string" ? this : this.closest('[rr-canvas]')
+                    break
+            }
+        }
+        document.addEventListener("navigate", navigateHandler)
     }
     attributeChangedCallback(name, oldValue, newValue) {
+        console.log("cat")
         if (name === "rr-line" && (oldValue !== newValue)) {
             this.dispatchEvent(new CustomEvent("scrolltoline", { detail: { line: newValue } }))
         }
@@ -168,3 +219,114 @@ class RrViewbox extends HTMLElement {
 }
 
 customElements.define("rr-viewbox", RrViewbox)
+
+class RrWorkspace extends HTMLElement {
+    getManifest() {
+        return (this.manifest || this.closest('[rr-manifest]'))
+            ? this.manifest || this.closest('[rr-manifest]').getAttribute("rr-manifest")
+            : ""
+    }
+    getCanvas() {
+        return (this.canvas || this.closest('[rr-canvas]'))
+            ? this.canvas || this.closest('[rr-canvas]').getAttribute("rr-canvas")
+            : ""
+    }
+    getSelector() {
+        return (this.selector || this.closest('[rr-selector]'))
+            ? this.selector || this.closest('[rr-selector]').getAttribute("rr-selector")
+            : ""
+    }
+    getLine() {
+        return (this.line || this.closest('[rr-line]'))
+            ? this.line || this.closest('[rr-line]').getAttribute("rr-line")
+            : ""
+    }
+    constructor() {
+        super()
+
+        this.attachShadow({ mode: 'open' })
+
+        this.manifest = this.getManifest()
+        this.selector = this.getSelector()
+        this.canvas = this.getCanvas()
+        this.line = this.getLine()
+        this.presentation = {}
+
+        this.bufferPixels = 15
+
+        this.addEventListener("scrolltoline", this.viewLine)
+
+        const shadowStyle = document.createElement("style")
+        shadowStyle.textContent = `
+        workspace {
+            display: block;
+            background-color: #ffffff;
+            width: 100vw;
+            flex: 1 1 auto;
+            box-shadow: 0 0 1em rgba(0,0,0,.5);
+            z-index: 5;
+        }`
+        this.shadowRoot.appendChild(shadowStyle)
+        const transcriptlet = document.createElement("textarea")
+        transcriptlet.value = this.text
+    }
+    connectedCallback() {
+
+        if (this.presentation.manifest) {
+            console.log("already loaded manifest")
+        } else {
+            fetch(this.manifest)
+                .then(response => {
+                    if (response.ok) {
+                        return response.json()
+                    } else {
+                        throw Error(response.statusText)
+                    }
+                })
+                .then(manifest => {
+                    this.presentation.manifest = manifest
+                    this.presentation.canvas = this.getCanvas() || manifest.sequences[0].canvases[0]
+                    this.text = this.presentation.canvas.otherContent[0].resources[0].resource['cnt:chars']
+                })
+        }
+        const bkBtn = document.createElement("button")
+        const fwdBtn = document.createElement("button")
+        [bkBtn, fwdBtn].forEach(b => b.setAttribute("role", "button"))
+        bkBtn.addEventListener("click")
+        this.shadowRoot.append(bkBtn, fwdBtn)
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === "rr-line" && (oldValue !== newValue)) {
+            const newLocal = "scrolltoline"
+            this.dispatchEvent(new CustomEvent(newLocal, { detail: { line: newValue } }))
+        }
+    }
+
+    resolveLine(lineId = this.getLine()) {
+        for (const line of this.presentation.canvas.otherContent[0].resources) {
+            if (line['@id'] === lineId) return line
+        }
+        return this.presentation.canvas.otherContent[0].resources[0]
+    }
+
+    viewLine(event) {
+        const line = this.resolveLine(event && event.detail.line)
+        this.selector = (line.on || line.target).split("xywh=")[1]
+        const viewportWidth = this.clientWidth
+        let [x, y, w, h] = this.selector.split(",").map(d => parseInt(d))
+        if (this.show === "below") {
+            y += h
+        }
+        const selectorToPage = w / viewportWidth
+        const imgFrame = this.shadowRoot.querySelector(".img-frame")
+        imgFrame.style.width = `${this.presentation.canvas.width / selectorToPage}px`
+        imgFrame.style.top = `-${y / selectorToPage}px`
+        imgFrame.style.left = `-${x / selectorToPage}px`
+        if (this.show === "self") {
+            this.style.height = `${h * viewportWidth / w}px`
+        }
+    }
+}
+
+customElements.define("rr-workspace", RrWorkspace)
+
--- public/transcribe/transcribe-utils.js
+++ public/transcribe/transcribe-utils.js
@@ -1,1 +1,30 @@
+const prevLineButtons = document.getElementsByClassName("selectPreviousLine")
+const nextLineButtons = document.getElementsByClassName("selectNextLine")
 
+if (prevLineButtons.length) {
+    Array.from(prevLineButtons).forEach(el=>el.addEventListener("click",navigateBackOneLine))
+}
+if (nextLineButtons.length) {
+    Array.from(nextLineButtons).forEach(el=>el.addEventListener("click",navigateForwardOneLine))
+}
+
+function navigateBackOneLine(event) {
+    navigateToLine("line","last",event.target)
+}
+
+function navigateForwardOneLine(event) {
+    navigateToLine("line","next",event.target)
+}
+
+function navigateToLine(type,line,el=document) {
+    const NavigateEvent = new CustomEvent("navigate",{
+        bubbles: true,
+        detail:{
+            goto: {
+                id: line,
+                type: type
+            }
+        }
+    })
+    el.dispatchEvent(NavigateEvent)
+}
