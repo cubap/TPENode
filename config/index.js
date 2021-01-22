ab0a4a9b072aeec26b12c2d6bbf5bdf1f83f84e3
Patrick Cuba <cubap@slu.edu>
2021-01-22 16:09:21 -0600

    Update index.js

--- config/index.js
+++ config/index.js
@@ -5,16 +5,22 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const envFound = dotenv.config();
 if (envFound.error) {
-  // This error should crash whole process
-  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+  // Don't crash whole process
+  // throw new Error("⚠️  Couldn't find .env file  ⚠️");
+  export default {
+    api: {
+      prefix: '/api',
+    }
+  }
+} else {
+  export default {
+    port: parseInt(process.env.PORT, 10),
+    databaseURL: process.env.MONGODB_URI,
+    jwtSecret: process.env.JWT_SECRET,
+    jwtAlgorithm: process.env.JWT_ALGO,
+    api: {
+      prefix: '/api',
+    }
+  }
 }
 
-export default {
-  port: parseInt(process.env.PORT, 10),
-  databaseURL: process.env.MONGODB_URI,
-  jwtSecret: process.env.JWT_SECRET,
-  jwtAlgorithm: process.env.JWT_ALGO,
-  api: {
-    prefix: '/api',
-  }
-};
