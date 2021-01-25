ddafa26d197ccd9506957c55f55e697bd393fd2e
Patrick Cuba <cubap@slu.edu>
2021-01-25 11:19:47 -0600

    come on hero
    ku

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import app from './express.js'
 
 async function startServer(port) {
   
-    app.listen(config.port, () => {
+    app.listen(process.env.port || config.port || 666, () => {
         console.log(`Example app listening at http://localhost:${port}`)
     })
 }
